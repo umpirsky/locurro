5fecd30c258f3049f0471d390eccae5382c802e2
umpirsky <umpirsky@gmail.com>
2015-08-01 15:11:45 +0200

    Currency provider chainig

---
+++ examples/ip-chained.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+$converter = new Locurro\Converter\IpAddress(
+    new Locurro\Converter\Country(
+        new Locurro\Converter\Locale(
+            new Locurro\Converter\Currency(
+                new Swap\Swap(
+                    new Swap\Provider\ChainProvider([
+                        new Swap\Provider\YahooFinanceProvider($httpAdapter = new Ivory\HttpAdapter\FileGetContentsHttpAdapter()),
+                        new Swap\Provider\GoogleFinanceProvider($httpAdapter),
+                        new Swap\Provider\WebserviceXProvider($httpAdapter)
+                    ])
+                )
+            )
+        )
+    ),
+    new Geocoder\Provider\GeoIP2(
+        new Geocoder\Adapter\GeoIP2Adapter(
+            new GeoIp2\Database\Reader(__DIR__.'/../data/GeoLite2-Country.mmdb'),
+            Geocoder\Adapter\GeoIP2Adapter::GEOIP2_MODEL_COUNTRY
+        )
+    )
+);
+
+$money = $converter->convert(
+    new Money\Money(100, new Money\Currency('EUR')),
+    '109.92.115.78'
+);
+
+var_dump($money);
